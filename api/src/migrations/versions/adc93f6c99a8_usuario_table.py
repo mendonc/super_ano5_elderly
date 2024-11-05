8dcd2d2296ec72c0b93bf3eb806b64dbb372770a
seb4stians <sebastiansilva@super.ufam.edu.br>
2024-11-05 01:48:31 -0400

    Tabela do usuario

--- api/src/migrations/versions/adc93f6c99a8_usuario_table.py
+++ api/src/migrations/versions/adc93f6c99a8_usuario_table.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'adc93f6c99a8'
-down_revision = '47df0f44f923'
+down_revision = None
 branch_labels = None
 depends_on = None
 
