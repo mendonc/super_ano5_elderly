37cccf1212ef33178fea01d202b14d858d57fc47
seb4stians <sebastiansilva@super.ufam.edu.br>
2024-11-05 01:47:39 -0400

    Tabelas do medico

--- api/src/migrations/versions/7000497fa020_medico_table.py
+++ api/src/migrations/versions/7000497fa020_medico_table.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '7000497fa020'
-down_revision = '47df0f44f923'
+down_revision = None
 branch_labels = None
 depends_on = None
 
