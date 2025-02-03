57ce16b9c4d2db7d316c6e1391ff9374722a9a12
Acsa <acsasant@gmail.com>
2025-02-02 22:42:02 -0400

    Removendo arquivo do Firebase do repositório

---
+++ health_app_01/backend/database.py
@@ -0,0 +1,11 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# Inicializa o Firebase
+cred = credentials.Certificate("C:/Users/pltmt/OneDrive/Documentos/APP/health_app_01/backend/firebase-key.json")
+
+firebase_admin.initialize_app(cred)
+
+# Instância do Firestore
+db = firestore.client()
+
